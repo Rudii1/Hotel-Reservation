0f435a21844e76657e6e2986b03273b06464756d
Fekete Rudolf Dominik <feketerudolf956@gmail.com>
2024-12-05 16:14:43 +0100

    Add aditional commits

---
+++ src/test/java/com/codeRudi/HotelServer/HotelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeRudi.HotelServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
