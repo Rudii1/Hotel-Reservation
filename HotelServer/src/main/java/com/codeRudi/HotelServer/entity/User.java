4e2fa4caac353288622631cd390071fae512cadf
Fekete Rudolf Dominik <feketerudolf956@gmail.com>
2024-12-12 00:31:19 +0100

    Adding UserRepo,AuthService,AuthImpl and fixing bugs

--- HotelServer/src/main/java/com/codeRudi/HotelServer/entity/User.java
+++ HotelServer/src/main/java/com/codeRudi/HotelServer/entity/User.java
@@ -21,28 +21,22 @@ public class User implements UserDetails {
     private Long id;
 
     private String email;
-
     private String password;
-
     private String name;
-
     private UserRole userRole;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(userRole.name()));
     }
-
     @Override
     public String getUsername() {
         return email;
     }
-
     @Override
     public String getPassword() {
         return password;
     }
-
 
     @Override
     public boolean isAccountNonExpired() {
---
+++ HotelServer/src/main/java/com/codeRudi/HotelServer/services/auth/AuthService.java
@@ -0,0 +1,4 @@
+package com.codeRudi.HotelServer.services.auth;
+
+public interface AuthService {
+}
