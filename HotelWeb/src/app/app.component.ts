dec146b6b6c23cd3f2d3570e8422e9e9d1032e20
Fekete Rudolf Dominik <feketerudolf956@gmail.com>
2024-12-18 22:49:28 +0100

    Signup page part.1

--- HotelWeb/src/app/app.component.ts
+++ HotelWeb/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgZorroModules } from './ngzorromodules'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NzLayoutModule,ReactiveFormsModule,NzButtonModule,NzInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
